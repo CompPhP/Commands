7677334a4794dd5722ca986b8632f87be40a07a6
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-09-30 12:35:30 +0530

    Create .php

---
+++ .php
@@ -0,0 +1,22 @@
+<?php
+    echo'Hello World';
+    echo nl2br("\r\n");
+    echo('String length of "Hello World: "');
+    echo strlen("Hello World");
+    echo nl2br("\r\n");
+    echo('String reverse of "Hello World: "');
+    echo strrev("Hello World");
+    echo nl2br("\r\n");
+    echo('String length of "Hello World: "');
+    echo strlen("Hello World");
+    echo nl2br("\r\n");
+    echo('Word Count of "Hello World: "');
+    echo str_word_count("Hello World");
+    echo nl2br("\r\n");
+    echo('String length of "Hello World: "');
+    echo strlen("Hello World");
+    echo nl2br("\r\n");
+    echo('String replace of "Hello World: "');
+    echo str_replace("Hello", "Bye", "Hello World");
+    
+?>
